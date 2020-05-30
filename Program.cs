55f396ba925270e38f091bed856cbba2279ee214
Wilson3193 <wilson@isppro.net>
2020-05-30 16:17:51 -0600

    test repository

--- Program.cs
+++ Program.cs
@@ -21,13 +21,13 @@ namespace ConsoleBrontoLeagues
             {
                 BrontoResult ObjBrontoJazz = new BrontoResult();
                 ObjBrontoJazz = new GenericHttpJson().PostEventResponse<BrontoResult, BrontoResult>(ObjBrontoJazz, "https://localhost:44346/api/Bronto/LeaguesProccessJazz ", " ");
-                Console.WriteLine("=========================Jazz SportsBook=======================");
+                Console.WriteLine("=========================Jazz SportsBook========================");
                 Console.WriteLine(ObjBrontoJazz.ToString());
-                Console.WriteLine("==========================ABC Islands==========================");
+                Console.WriteLine("==========================ABC Islands===========================");
                 BrontoResult ObjBrontoABC = new BrontoResult();
                 ObjBrontoABC = new GenericHttpJson().PostEventResponse<BrontoResult, BrontoResult>(ObjBrontoABC, "https://localhost:44346/api/Bronto/LeaguesProccessABC ", " ");
                 Console.WriteLine(ObjBrontoABC.ToString());
-                Console.WriteLine("==========================Loose lines==========================");
+                Console.WriteLine("==========================Loose lines===========================");
                 BrontoResult ObjBrontoLooseLines = new BrontoResult();
                 ObjBrontoLooseLines = new GenericHttpJson().PostEventResponse<BrontoResult, BrontoResult>(ObjBrontoLooseLines, "https://localhost:44346/api/Bronto/LeaguesProccessLooseLines ", " ");
                 Console.WriteLine(ObjBrontoLooseLines.ToString());
